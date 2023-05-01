7abbae679152923b581913d115b67b3f9890cf70
SobolevicDaniell <sobolevdan99@yandex.ru>
2023-05-01 22:06:51 +0300

    First bild

--- NINJA_DELIVERY/Assets/Scripts/Muvment.cs
+++ NINJA_DELIVERY/Assets/Scripts/Muvment.cs
@@ -15,11 +15,14 @@ public class Muvment : MonoBehaviour
         [SerializeField] private float groundCheckRadius = 0.2f;
         
         private Rigidbody2D rb;
-        [SerializeField] private bool isGrounded;
+        [SerializeField] public bool isGrounded;
+
+        private Animator anim;
 
         void Start()
         {
             rb = GetComponent<Rigidbody2D>();
+            anim = GetComponent<Animator>();
         }
 
         void FixedUpdate()
@@ -33,6 +36,7 @@ public class Muvment : MonoBehaviour
             ChangeVector();
             GrounCheck();
             Jump();
+            Tackles();
         }
         
         
@@ -52,12 +56,12 @@ public class Muvment : MonoBehaviour
 
         private void ChangeVector()
         {
-            if(transform.position.x > triggerR.position.x && isMovingRight && Input.GetKeyDown(KeyCode.A))
+            if(transform.position.x > triggerR.position.x && isMovingRight && Input.GetKey(KeyCode.A))
             {
                 isMovingRight = false;
                 transform.localScale = new Vector3(-1, 1, 1);
             }
-            else if(transform.position.x < triggerL.position.x && !isMovingRight && Input.GetKeyDown(KeyCode.D))
+            else if(transform.position.x < triggerL.position.x && !isMovingRight && Input.GetKey(KeyCode.D))
             {
                 isMovingRight = true;
                 transform.localScale = new Vector3(1, 1, 1);
@@ -67,7 +71,8 @@ public class Muvment : MonoBehaviour
         private void GrounCheck()
         {
             isGrounded = Physics2D.OverlapCircle(groundCheckTransform.position, groundCheckRadius, groundLayerMask);
-
+            anim.SetBool("onGround", isGrounded);
+            
         }
 
         private void Jump()
@@ -75,6 +80,18 @@ public class Muvment : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
             {
                 rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            }
+        }
+
+        private void Tackles()
+        {
+            if (Input.GetKey(KeyCode.LeftControl) && isGrounded)
+            {
+                anim.SetBool("tackles", true);
+            }
+            else
+            {
+                anim.SetBool("tackles", false);
             }
         }
     }
